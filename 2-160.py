22b7503c41cb1b955e9c2ca41f877cfea0077f01
Anurag Pal <palanurag595@gmail.com>
2025-04-01 23:21:56 +0530

    call

--- 2-160.py
+++ 2-160.py
@@ -17,3 +17,8 @@ def pushZerosToEnd(arr):
             # Move 'count' pointer to the next position
             count += 1
 
+if __name__ == "__main__":
+    arr = [1, 2, 0, 4, 3, 0, 5, 0]
+    pushZerosToEnd(arr)
+    for num in arr:
+        print(num, end=" ")
