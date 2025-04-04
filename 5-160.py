7c9c88e3512def8bba35bdc003128872532a4377
Anurag Pal <palanurag595@gmail.com>
2025-04-04 23:02:39 +0530

    completed

--- 5-160.py
+++ 5-160.py
@@ -1,12 +1,15 @@
-class solution:
-    def nextpermutation(self, arr):
-        n = len(arr)
+#User function Template for python3
 
+class Solution:
+    def nextPermutation(self, arr):
+        n = len(arr)
+        
         pivot = -1
         for i in range(n - 2, -1, -1):
             if arr[i] < arr[i + 1]:
                 pivot = i
-
+                break
+                
         if pivot == -1:
             arr.reverse()
             return
@@ -15,9 +18,29 @@ class solution:
             if arr[i] > arr[pivot]:
                 arr[i], arr[pivot] = arr[pivot], arr[i]
                 break
+            
+        left, right = pivot + 1, n - 1
+        while left < right:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
 
-            left, right = pivot + 1, n - 1
-            while left <= right:
-                arr[left], arr[right] = arr[right], arr[left]
-                left += 1
-                right -= 1
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        arr = input().split()
+        N = len(arr)
+        for i in range(N):
+            arr[i] = int(arr[i])
+
+        ob = Solution()
+        ob.nextPermutation(arr)
+        for i in range(N):
+            print(arr[i], end=" ")
+        print()
+
+# } Driver Code Ends
